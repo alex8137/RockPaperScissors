ea3583416e64361298be9db624c861717ef587bd
Alex <sidney840721@gmail.com>
2022-09-02 15:12:25 +0800

    All Game is Done with Basic Version

--- javascript.js
+++ javascript.js
@@ -17,18 +17,49 @@ function playRound(playerSelection, computerSelection){
   let player = playerPart1 + playerPart2;
 
   if (player==="Rock" && computerSelection==="Scissors" || player==="Paper" && computerSelection==="Rock" || player==="Scissors" && computerSelection==="Paper"){
-    return `You Win! Your ${player} beats ${computerSelection}!`;
+    alert(`You Win! Your ${player} beats ${computerSelection}!`);
+    return "WIN";
   }else if (player===computerSelection){
-    return `Tie! Both choose same object, ${player}!`;
+    alert(`Tie! Both choose same object, ${player}!`);
+    return "TIE";
   }else {
-    return `You Lose! Your ${player} is beaten by ${computerSelection}!`;
+    alert(`You Lose! Your ${player} is beaten by ${computerSelection}!`);
+    return "LOSE";
   }
 }
 
+function game(){
+  // Let's start a 5 rounds game.
+  let yourScore = 0;
+  let computerScore = 0;
 
+  for (i = 0; i < 5; i++){
+    let playerSelection = prompt(`What do you want to choose in round${i+1}?`, "Rock, Paper, or Scissors");
+    while((playerSelection.toLowerCase() !== "rock") && (playerSelection.toLowerCase() !== "paper") && (playerSelection.toLowerCase() !== "scissors")){
+      alert("You typed in wrong format. You can only type 'rock', 'paper', 'scissors'. Please retype again.");
+      playerSelection = prompt(`What do you want to choose in round${i+1}?`, "Rock, Paper, or Scissors");
+    }
+    
+    let computerSelection = getComputerChoice();
+    let result = playRound(playerSelection, computerSelection);
 
+    if (result === "WIN"){
+      yourScore++;
+    }else if (result === "TIE"){
+      yourScore++;
+      computerScore++;
+    }else {
+      computerScore++;
+    }
+    alert(`Your score ${yourScore}:${computerScore} Computer's score`);
+  }
 
+  if (yourScore > computerScore){
+    alert(`YOU WIN!! With scores ${yourScore}:${computerScore}!!`);
+  }else if (yourScore === computerScore){
+    alert(`TIE!! Both have same scores ${yourScore}:${computerScore}!!`);
+  }else {
+    alert(`YOU LOSE!! With scores ${yourScore}:${computerScore}!!`);
+  }
+}
 
-const playerSelection = "paPEr";
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
